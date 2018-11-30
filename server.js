923a5f7a6bbe9edaa38e24c723d8b75c2ef235b1
Anabela Nogueira <anabela.nogueira@jscrambler.com>
2018-11-30 18:08:13 +0000

    final update server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 
 var express = require('express');
 var cors = require('cors');
-
-// require and use "multer"...
+var multer = require('multer');
 
 var app = express();
+var upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -14,9 +14,16 @@ app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
-});
+  app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => { 
+    if (req.file) {
+      res.json({
+        filename: req.file.originalname,
+        size: req.file.size
+      });
+    } else {
+      res.send('No file uploaded.');
+    }
+  });
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
